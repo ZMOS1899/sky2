752fba083bc99f504d3be0856aeeaba3a6f2dfff
Zmos <zhangmos1899@gmail.com>
2024-02-07 03:16:17 +0100

    店铺营业状态设置 code

---
+++ sky-server/src/main/java/com/sky/service/impl/ShopServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sky.service.impl;
+
+import com.sky.service.ShopService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopServiceImpl implements ShopService {
+
+
+    @Override
+    public void setStatus(Integer status) {
+
+    }
+}
